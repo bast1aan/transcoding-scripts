88dd79e444df27a3a9a9fc533de308493f2efba1
Bastiaan Welmers <bastiaan@welmers.net>
2020-11-22 17:48:05 +0100

    480p and 360p as well

--- 2018/transcode_h264.sh
+++ 2018/transcode_h264.sh
@@ -2,3 +2,6 @@ ffmpeg -i VID00081.MOV -acodec copy -vcodec libx264 -level 21 -refs 2 -b:v 2048k
 
 ffmpeg -i lund-universitetsbibliothek-stortorget.mp4 -acodec copy -vcodec libx264 -level 21 -refs 2 -b:v 4096k -threads 0 -s 1280x720 web/lund-universitetsbibliothek-stortorget-720p.mp4
 
+ffmpeg -i lund-universitetsbibliothek-stortorget.mp4 -acodec copy -vcodec libx264 -level 21 -refs 2 -b:v 1792k -threads 0 -s 853x480 web/lund-universitetsbibliothek-stortorget-480p.mp4
+
+ffmpeg -i lund-universitetsbibliothek-stortorget.mp4 -acodec copy -vcodec libx264 -level 21 -refs 2 -b:v 1024k -threads 0 -s 640x360 web/lund-universitetsbibliothek-stortorget-360p.mp4
