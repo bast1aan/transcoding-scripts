2a9c0d25656e18f181734b223a82f3495957e97d
Bastiaan Welmers <bastiaan@welmers.net>
2022-04-26 22:25:26 +0200

    transcode_vp8-crf25.sh

---
+++ 2022/transcode_vp8-crf25.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+_threads() {
+	# vp8 needs > 0 threads
+	if [ "$threads" -le 1 ]; then
+		threads="4"
+	fi
+}
+
+run_480() {
+	_threads
+	ffmpeg -i $in -acodec libvorbis -ac 2 -b:a 96k -vcodec libvpx -b:v 0 -crf 25 -threads $threads -s 854x480 $frame_rate $base.480p-vp8.webm
+}
+
+run_360() {
+	_threads
+	ffmpeg -i $in -acodec libvorbis -ac 2 -b:a 96k -vcodec libvpx -b:v 0 -crf 25 -threads $threads -s 640x360 $frame_rate $base.360p-vp8.webm
+}
+
+run_720() {
+	_threads
+	ffmpeg -i $in -acodec libvorbis -ac 2 -b:a 96k -vcodec libvpx -b:v 0 -crf 25 -threads $threads -s 1280x720 $frame_rate $base.720p-vp8.webm
+}
+
+run_1080() {
+	_threads
+	ffmpeg -i $in -acodec libvorbis -ac 2 -b:a 96k -vcodec libvpx -b:v 0 -crf 25 -threads $threads -s 1920x1080 $frame_rate $base.1080p-vp8.webm
+}
+
+. ./base.sh
